5b71429b14ff0250c7d8305d5af85d9c90faab68
Caleb Kahan <ckahan@tamu.edu>
2025-10-31 11:52:54 -0500

    add riboflavin testing

--- TestsCpp.R
+++ TestsCpp.R
@@ -181,21 +181,23 @@ bench_result <- microbenchmark(
 
 # Tests on riboflavin data
 ##########################
-# require(hdi) # this should install hdi package if you don't have it already; otherwise library(hdi)
-# data(riboflavin) # this puts list with name riboflavin into the R environment, y - outcome, x - gene erpression
-# 
-# # Make sure riboflavin$x is treated as matrix later in the code for faster computations
-# class(riboflavin$x) <- class(riboflavin$x)[-match("AsIs", class(riboflavin$x))]
-# 
-# # Standardize the data
-# out <- standardizeXY(riboflavin$x, riboflavin$y)
-# 
-# # This is just to create lambda_seq, can be done faster, but this is simpler
-# outl <- fitLASSOstandardized_seq(out$Xtilde, out$Ytilde, n_lambda = 30)
-# 
-# # The code below should assess your speed improvement on riboflavin data
-# microbenchmark(
-#   fitLASSOstandardized_seq(out$Xtilde, out$Ytilde, outl$lambda_seq),
-#   fitLASSOstandardized_seq_c(out$Xtilde, out$Ytilde, outl$lambda_seq),
-#   times = 10
-# )
+require(hdi) # this should install hdi package if you don't have it already; otherwise library(hdi)
+data(riboflavin) # this puts list with name riboflavin into the R environment, y - outcome, x - gene erpression
+
+# Make sure riboflavin$x is treated as matrix later in the code for faster computations
+class(riboflavin$x) <- class(riboflavin$x)[-match("AsIs", class(riboflavin$x))]
+
+# Standardize the data
+out <- standardizeXY(riboflavin$x, riboflavin$y)
+
+# This is just to create lambda_seq, can be done faster, but this is simpler
+outl <- fitLASSOstandardized_seq(out$Xtilde, out$Ytilde, n_lambda = 30)
+
+# The code below should assess your speed improvement on riboflavin data
+microbenchmark(
+  fitLASSOstandardized_seq(out$Xtilde, out$Ytilde, outl$lambda_seq),
+  fitLASSOstandardized_seq_c(out$Xtilde, out$Ytilde, outl$lambda_seq),
+  times = 10
+)
+# R-version: 1600 milliseconds median
+# C-version: 20 milliseconds median
